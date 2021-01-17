ce976b1dcb05837a13a64de1e87546c32ff71c7f
yusuke <yuusuke_kotani@dwango.co.jp>
2021-01-17 13:11:46 +0900

    changes for heroku

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -2,7 +2,7 @@
 'use strict';
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(
-	process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/nonleak'
+	 process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/nonleak'
 );
 
 module.exports = {
