3e2eb690b0bc0cf8d761a17bb5f6bdca5c7947a9
yusuke <yuusuke_kotani@dwango.co.jp>
2021-01-19 16:47:32 +0900

    change for debug

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -4,8 +4,6 @@ const Sequelize = require('sequelize');
 console.log("-----------------------------------")
 console.log(process.env.DATABASE_URL)
 console.log("------------------------------------------------")
-console.log(process.env)
-console.log("---------------------------------------")
 const sequelize = new Sequelize(
 	 process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/nonleak'
 );
