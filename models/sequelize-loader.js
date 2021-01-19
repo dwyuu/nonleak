25fbfbe422ebeb0f51dffd0f59c29dc655179e57
yusuke <yuusuke_kotani@dwango.co.jp>
2021-01-19 15:35:04 +0900

    changes for debug

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -10,6 +10,13 @@ const sequelize = new Sequelize(
 	 process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/nonleak'
 );
 
+try {
+	await sequelize.authenticate();
+	console.log('Connection has been established successfully.');
+  } catch (error) {
+	console.error('Unable to connect to the database:', error);
+  }
+
 module.exports = {
 	  database: sequelize,
 	  Sequelize: Sequelize
