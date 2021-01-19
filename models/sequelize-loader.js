a80409912a5b5e2385e85d5942b9af5fb8025143
yusuke <yuusuke_kotani@dwango.co.jp>
2021-01-19 20:59:58 +0900

    changes for heroku

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -5,8 +5,11 @@ console.log("-----------------------------------")
 console.log(process.env.DATABASE_URL)
 console.log("------------------------------------------------")
 const sequelize = new Sequelize(
-	 process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/nonleak'
+	 process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/nonleak',
+	 {dialectOptions:{ssl:true}}
 );
+
+
 
 
 module.exports = {
